d631d3b39b6dea095bc3f17a35cee947ff72dbc8
OyedunOye <oyesinaoyedun@yahoo.com>
2025-02-04 00:53:44 +0100

    feat:created users related endpoints
    The changes in this commit is focused on creating functional 'get' endpoint to see all users registered in the database, another get endpoint to view individual app user with their default id, and finally, a post endpoint to allow the creation of new users and saving their registration details to the database. These endpoints have been tested using Thunder Client VS Code extension and works perfectly.

--- app.js
+++ app.js
@@ -2,15 +2,20 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const morgan = require('morgan')
 // const port = 3003
 const userRouter = require("./src/routes/users.route");
-app.use(cors())
-app.use(express.json())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"))
 app.use("/api/users", userRouter)
 
 app.get('/', (req, res) => {
   res.send('Hello Shade!')
 })
+
 
 module.exports = app
--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -1,8 +1,31 @@
 const userRouter = require("express").Router();
+const bcrypt = require('bcrypt')
 const User = require("../models/user")
 userRouter.get("/", async (req, res) => {
     const allUsers = await User.find({})
     res.json(allUsers)
 })
+
+userRouter.get("/:id", async (req, res) => {
+    const id = req.params.id
+    const user = await User.findById(id)
+    res.json(user)
+}) 
+
+userRouter.post("/", async (req, res) => {
+    const { firstName, lastName, email, password } = req.body
+    try {
+        const salt = 10
+        // console.log(req)
+        // console.log(firstName, lastName, email, password)
+        // console.log(req.body)
+        const passwordHash = await bcrypt.hashSync(password, salt)
+        const newUser = new User({firstName, lastName, email, passwordHash})
+        const saveUser = await newUser.save()
+        res.status(201).json({user: saveUser, message: "User created Successfully!"})
+    } catch (error) {
+        console.log(error)
+    }
+}) 
 
 module.exports = userRouter
